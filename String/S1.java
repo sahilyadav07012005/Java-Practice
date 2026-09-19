89f9711bd881a9e5a34cf58061a76dba882b39dc
Sahil Yadav <yadavsahil5199@gmail.com>
2026-09-20 02:52:43 +0530

    Add files via upload

---
+++ String/S1.java
@@ -0,0 +1,11 @@
+public class S1 {
+    public static void main(String[] args) {
+
+        String name = "Sahil";
+
+        System.out.println("Name: " + name);
+        System.out.println("Length: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+    }
+}
